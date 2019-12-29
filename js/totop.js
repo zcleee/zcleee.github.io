528dd01823b1d2a6e6c63fd8f5d3fd275691de78
zclee <zcleepython@163.com>
2019-12-29 11:31:03 +0000

    CI: modified layou

---
+++ js/totop.js
@@ -0,0 +1,19 @@
+(function(window, document) {
+  hljs.initHighlightingOnLoad();
+  $('#go-to-top').on('click', function() {
+	$("html, body").animate({
+	  "scrollTop": 0
+	}, 400);
+	return false;
+  });
+  $(window).scroll(function() {
+	let top = $(document).scrollTop();
+	let g = $("#go-to-top");
+	if (top > 300 && g.is(":hidden")) {
+	  g.fadeIn();
+	} else if (top < 300 && g.is(":visible")) {
+	  g.fadeOut();
+	}
+  });
+
+})(this, this.document);
